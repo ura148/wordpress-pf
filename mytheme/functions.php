54bc054e3f337aa2b5d250907d65456083b3b1cf
ura148 <yuki.ura10@gmail.com>
2020-04-20 17:59:38 +0900

    記事ページのレイアウト フォントの設定、記事のwidth設定

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -4,6 +4,19 @@
 
   //縦横比を維持したレスポンシブを有効化
   add_theme_support( 'responsive-embeds');
+
+  //エディタ用CSSの有効化、エディタに読み込み
+  add_theme_support( 'editor-styles' ); //???p120
+  add_editor_style( 'editor-style.css' );//???
+
+  //ページのタイトルを有効化
+  add_theme_support( 'title-tag' );
+
+  //link, style, script のHTML5対応を有効化
+  add_theme_support( 'html5', array(
+    'style',
+    'script'
+  ) );
 }
 add_action( 'after_setup_theme', 'mytheme_setup' );
 
--- mytheme/index.php
+++ mytheme/index.php
@@ -8,9 +8,18 @@
   <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <?php if(have_posts()): while(have_posts()):the_post(); ?>
-      <article <?php post_class(); ?>>
-        <?php the_title(); ?>
+      <article <?php post_class( 'mycontainer' ); ?>>
+        <div class="myposthead">
+          <?php the_category(); ?>
+          <h1><?php the_title(); ?></h1>
 
+          <!--投稿日時を取得-->
+          <time datetime="<?php echo esc_attr( get_the_date( DATE_w3c )); ?>">
+            <?php echo esc_html( get_the_date() ); ?>
+            <!-- echo は出力を意味する-->
+            <!-- 投稿日をget_the_date()で取得　esc_html()で無害化 -->
+          </time>
+        </div>
         <?php the_content(); ?>
       </article>
     <?php endwhile; endif; ?>
