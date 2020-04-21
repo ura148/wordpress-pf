42af9743a313994e964ab5149a1d40899796c97e
ura148 <yuki.ura10@gmail.com>
2020-04-21 09:59:37 +0900

    header,footerをテンプレートとして作成

--- mytheme/index.php
+++ mytheme/index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <?php  wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
+    <?php get_header(); ?>
 
-    <header class="myhead mycontainer">
-      <a href="<?php echo esc_url( home_url( '/' )); ?>">
-        <?php bloginfo( 'name' ); ?>
-      </a>
-
-      <p><?php bloginfo( 'description' ); ?></p>
-    </header>
     <?php if(have_posts()): while(have_posts()):the_post(); ?>
       <article <?php post_class( 'mycontainer' ); ?>>
         <div class="myposthead">
@@ -33,10 +18,5 @@
         <?php the_post_navigation(); ?>
       </article>
     <?php endwhile; endif; ?>
-
-    <footer class="myfoot">
-      <?php bloginfo ( 'name' ); ?>
-    </footer>
-    <?php wp_footer(); ?>
-  </body>
-</html>
+    
+    <?php get_footer(); ?>
